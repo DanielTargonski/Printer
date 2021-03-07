2a4f797c6cbd3532bbd9cd8560624a6f0bce2dce
Daniel Targonski <daniel.targonski2@gmail.com>
2021-03-06 21:13:32 -0500

    updates

--- Printer.java
+++ Printer.java
@@ -1,14 +1,8 @@
-/**
- * 
+/** A basic printer that will check for needed amount of ink and paper
+ * before printing requested task.
  */
 
 package edu.cuny.csi.csc330.lab4;
-
-import java.util.Arrays;
-import java.util.Date;
-
-import edu.cuny.csi.csc330.util.Randomizer;
-
 
 public class Printer {
 
@@ -16,53 +10,108 @@ public class Printer {
 	
 	public class InkCartridge {
 		
+		/** Will model a printer Ink Cartridge
+		 * 
+		 */
 		public InkCartridge() {
 			blackInkLeft = 100;
 			redInkLeft = 100;
 			greenInkLeft = 100;
 			blueInkLeft = 100;
+			
+			
 		}		
 		
+
 		private double blackInkLeft;
 		private double redInkLeft;
 		private double greenInkLeft;
 		private double blueInkLeft;
 		
-		public void getInkStatus() {
+		/**
+		 * Outputs all the remaining ink levels.
+		 */
+		private void getInkStatus() {
 			System.out.println("Remaining ink levels are: \n"
 					+ "Blue Ink:  " + inkCartridge.blueInkLeft + "%\n"
 					+ "Green Ink: " + inkCartridge.greenInkLeft + "%\n"
 					+ "Red Ink:   " + inkCartridge.redInkLeft + "%\n"
-					+ "Black Ink: " + inkCartridge.blackInkLeft);
+					+ "Black Ink: " + inkCartridge.blackInkLeft + "%");
 		}
 		
-		public void refillInk() {
+		/**
+		 *  Refills inks levels of ink cartridge.
+		 */
+		private void refillInk() {
 			blackInkLeft = 100;
 			redInkLeft = 100;
 			greenInkLeft = 100;
 			blueInkLeft = 100;
+			System.out.println("All Ink has been filled to 100%!\n");
 		}
+		
+		@Override
+		public String toString() {
+			return " [blackInkLeft=" + blackInkLeft + ", redInkLeft=" + redInkLeft + ", greenInkLeft="
+					+ greenInkLeft + ", blueInkLeft=" + blueInkLeft + "]";
+		}
+
 	} // end class InkCartridge
 	
-	
-	public class PaperCartridge {
+	/** Models a printer paper tray.
+	 *
+	 */
+	public class PaperTray {
 		
-		public PaperCartridge() {
+		public PaperTray() {
 			numberOfPapers = maxNumOfPapers;
 			isOpen = false;
 		}
 		
+
 		private boolean isOpen;
 		private double numberOfPapers;
-
 		private double maxNumOfPapers = 500;
 		
-		public void displayNumberOfPapers() {
+		/**
+		 * Opens the paper tray.
+		 */
+		private void openPaperTray () {
+			isOpen = true;
+			System.out.println("Paper tray is open.");
+		}
+		
+		/**
+		 * Closes the paper tray.
+		 */
+		private void closePaperTray () {
+			isOpen = false;
+			System.out.println("Paper tray is closed.");
+		}
+		
+		/**
+		 * 
+		 */
+		private void displayNumberOfPapers() {
 			System.out.println("Number of pages remaining: " + numberOfPapers);
 		}
-		public void refillPaper() {
+		private void refillPaper() {
+			openPaperTray();
+			
 			numberOfPapers = maxNumOfPapers;
+			System.out.println("Paper has been refilled to " + maxNumOfPapers + " pages.");
+			
+			closePaperTray();
+			
+			System.out.println();
 		}
+		
+		@Override
+		public String toString() {
+			return " [isOpen=" + isOpen + ", numberOfPapers=" + numberOfPapers + ", maxNumOfPapers="
+					+ maxNumOfPapers + "]";
+		}
+
 
 	} // end class PaperCartridge
 	
@@ -72,50 +121,72 @@ public class Printer {
 	public Printer() {
 		isOn = false;
 		setPrinterName("Printer");
+		System.out.println("New Printer instance created.");
 	}
-
+	public Printer(String printerName) {
+		isOn = false;
+		setPrinterName(printerName);
+		System.out.println("New Printer instance created.");
+	}
 	// ------------------------- END Constructors -------------------------
 	
 	
 	
-	// ------------------------- Members -------------------------
+	// ------------------------- Members/State -------------------------
 	private boolean isOn;
 	private String printerName;
-
-	PaperCartridge paperCartridge = new PaperCartridge();
-	InkCartridge inkCartridge = new InkCartridge();
+	private PaperTray paperCartridge = new PaperTray();
+	private InkCartridge inkCartridge = new InkCartridge();
 	
 	// ------------------------- END Members -------------------------
 	
 	// ------------------------- Methods -------------------------
 	
+	/**
+	 * getter for the printer references name.
+	 * @return printerName
+	 */
 	public String getPrinterName() {
 		return printerName;
 	}
 
+	/**
+	 * setter for the printer name.
+	 * @param printerName:String sets the printerName
+	 */
 	public void setPrinterName(String printerName) {
 		this.printerName = printerName;
 	}
 
+	/**
+	 * Turns on the printer.
+	 */
 	public void turnOn() {
 		isOn = true;
-		System.out.println(this.getPrinterName() + " is now on.");
+		System.out.println(this.getPrinterName() + " is now on.\n");
 	}
 	
+	/**
+	 * Turns off the printer.
+	 */
 	public void turnOff() {
 		isOn = false;
 	}
 	
+	/**
+	 * Checks to see if the printer is on.
+	 * @return boolean value, true if the printer is on and vice-versa.
+	 */
 	public boolean isOn() {
 		return isOn;
 	}
 	
-	/**
+	/** Prints color pages.
 	 * @param numOfCopies
 	 * @return true if print was successful
 	 */
 	public boolean printColor(int numOfCopies) {
-		if (!checkForEnough(numOfCopies, false))
+		if (!checkForEnough(numOfCopies, true))
 			return false;
 		
 		System.out.println("Printing " + numOfCopies + " pages in color.");
@@ -128,12 +199,17 @@ public class Printer {
 		inkCartridge.getInkStatus();
 		paperCartridge.displayNumberOfPapers();
 		
-		System.out.println();
+		System.out.println("Color print successful!\n");
 		
 		return true;
 				
 	} // end printColor()
 	
+	/**
+	 * Prints black and white pages.
+	 * @param numOfCopies:int  the number of copies to be printed.
+	 * @return boolean value, true if print was successful.
+	 */
 	public boolean printBlackandWhite(int numOfCopies) {
 		if (!checkForEnough(numOfCopies, false))
 			return false;
@@ -147,60 +223,111 @@ public class Printer {
 		inkCartridge.getInkStatus();
 		paperCartridge.displayNumberOfPapers();
 		
-		System.out.println();
+		System.out.println("Black print successful!\n");
 		
 		return true;
 	}
 	
+	/** checks the printer to see if it is on, and if it has enough ink and paper for the 
+	 * requested printing job.
+	 * 
+	 * @param numOfCopies:int  the number of copies to check if printer has enough paper and ink for.
+	 * @param color:boolean  pass true if you want to check for color printing, false for non-color printing
+	 * @return boolean  true if there is enough paper and ink to do the printing job.
+	 */
 	public boolean checkForEnough(int numOfCopies, boolean color) {
-		if (!this.isOn())
-		{
-			System.err.println("The printer is not on.\n");
+		
+		if (!this.isOn()) {
+			System.err.println("You requested a print job but " + printerName + " is not on.\n");
 			return false;
 		}
 		
+		System.out.println("Checking for required ink and paper for requested job...");
+
 		if ( (paperCartridge.numberOfPapers - numOfCopies) < 0) {
-			System.err.println("Printer either does not have enough ink or paper to print " + 
-				numOfCopies + " pages.\n");
+			System.err.println(printerName + " does not have enough paper to print " + 
+				numOfCopies + " pages.");
 			
 			paperCartridge.displayNumberOfPapers();
+			System.out.println("\n");
 			
 			return false;
 		}
 		
-		if ( color == false && (inkCartridge.blackInkLeft - (numOfCopies * 0.2) < 0)) {
-			System.err.println("Printer does not have enough black ink left to print " +
+		if ( color == false && ((inkCartridge.blackInkLeft - (numOfCopies * 0.2)) < 0)) {
+			System.err.println(printerName + " does not have enough black ink left to print " +
 		numOfCopies + " pages.\n");
 			
 			inkCartridge.getInkStatus();
+			System.out.println("\n");
 			
 			return false;
 		}
-		if ( color == true && (inkCartridge.blueInkLeft - (numOfCopies * 0.2) < 0)) {
-			System.err.println("Printer does not have enough color ink left to print " +
+		if ( color == true && ((inkCartridge.blueInkLeft - (numOfCopies * 0.2)) < 0)) {
+			System.err.println(printerName + " does not have enough color ink left to print " +
 		numOfCopies + " pages.\n");
 			
 			inkCartridge.getInkStatus();
+			System.out.println("\n");
 			
 			return false;
 		}
+		System.out.println("Check complete, " + printerName + " has required resources. Initiating print job...\n");
+		
 		return true;
 
 	}
 	
+	public void refillPaper() {
+		paperCartridge.refillPaper();
+	}
+	private void refillInk() {
+		inkCartridge.refillInk();
+	}
+
 	
+	@Override
+	public String toString() {
+		return "Printer [isOn=" + isOn + ", printerName=" + printerName + ",\npaperCartridge=" + paperCartridge
+				+ ",\ninkCartridge=" + inkCartridge + "]\n\n";
+	}
+
+
+
 	
 	// ------------------------- END Methods -------------------------
 	
 	
 	public static void main(String[] args) {
-		Printer printer = new Printer();
-		printer.printColor(500);
-		printer.turnOn();
-		printer.printColor(250);
-		printer.printColor(251);
 		
+		Printer printer = new Printer("Dan's Printer");
+
+		System.out.println(printer.toString());
+		
+		printer.printColor(500);
+		
+		printer.turnOn();
+		
+		printer.printColor(35);
+		
+		System.out.println(printer.toString());
+		
+		printer.printColor(465);
+		printer.printColor(31);
+		printer.refillPaper();
+		printer.printColor(5);
+		printer.refillInk();
+		printer.printColor(153);
+		
+		System.out.println(printer.toString());
+		
+		printer.printBlackandWhite(300);
+		printer.refillPaper();
 		printer.printBlackandWhite(250);
+		printer.refillInk();
+		printer.printBlackandWhite(250);
+		
+		System.out.println(printer.toString());
 		
 	}
 }
